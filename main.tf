e7ac2b4848240555e23b6a97a761660879272880
karolnedza <37583943+karolnedza@users.noreply.github.com>
2022-03-10 10:38:41 +0100

    Update main.tf
    Minor changes

--- main.tf
+++ main.tf
@@ -18,20 +18,11 @@ resource "time_sleep" "iam_resource_propagation" {
 #################################
 # Create Account on Controller
 
-resource "aviatrix_account" "pro7sat_iam_account_1" {
-  account_name       = "aws-pro7-some-account_1"
+resource "aviatrix_account" "avx_iam_account_1" {
+  account_name       = "avx-aws-some-account_1"
   cloud_type         = 1
   aws_account_number = module.aviatrix-iam-roles.aws-account
   aws_iam            = true
-  aws_role_app       = time_sleep.iam_resource_propagation.triggers["app_arn"]
+  aws_role_app       = time_sleep.iam_resource_propagation.triggers["app_arn"] # AWS tends to be slow with creating and propageting IAM roles 
   aws_role_ec2       = time_sleep.iam_resource_propagation.triggers["ec2_arn"]
 }
-
-# resource "aviatrix_account" "pro7sat_iam_account_2" {
-#   account_name       = "aws-pro7-some-account_2"
-#   cloud_type         = 1
-#   aws_account_number = module.aviatrix-iam-roles.aws-account
-#   aws_iam            = true
-#   aws_role_app       = time_sleep.iam_resource_propagation.triggers["app_arn"]
-#   aws_role_ec2       = time_sleep.iam_resource_propagation.triggers["ec2_arn"]
-# }
